7cebcff8a14958c5a0e4ba63e118e6d86c2fe34f
TimesNewRoman <chris.horan27@gmail.com>
2016-02-02 11:03:18 -0800

    Create Add.bat

---
+++ Add.bat
@@ -0,0 +1,19 @@
+REM Addition game where you must add 2 numbers between 1 and 100
+REM Written by: Chris Horan
+
+@echo off
+setlocal EnableDelayedExpansion
+
+:LOOP
+
+SET /a num1 = %RANDOM% %% 100 + 1
+SET /a num2 = %RANDOM% %% 100 + 1
+
+SET /a ans= %num1% + %num2%
+
+SET /p guess= %num1% + %num2% = 
+
+IF %guess% == %ans% echo Correct!
+IF NOT %guess% == %ans% echo Nope, %ans%
+
+GOTO LOOP
